afd1e30907d64481442d565fa3e17b2479ed39e0
CaoTuanKiett <tuankiet06032002@gmail.com>
2024-10-08 21:29:21 +0700

    Update database configuration and dependencies

---
+++ README.md
@@ -0,0 +1,7 @@
+## RUN APP
+
+`uvicorn app.main:app --reload`
+
+## INSTALL PACKAGE
+
+`pip install -r requirements.txt`
--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
 from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 class Settings(BaseSettings):
     database_url: str = "mysql+mysqlclient://user:password@localhost/dbname"
